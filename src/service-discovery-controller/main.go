a1f6fe36009d2176d2e43670813b974f94297d7c
Dennis Leon <dleon@pivotal.io>
2017-10-04 15:11:41 -0700

    Add logic to SDC to return hardcoded responses
    [#151111319]
    
    Signed-off-by: Tyler Schultz <tschultz@pivotal.io>

--- src/service-discovery-controller/main.go
+++ src/service-discovery-controller/main.go
@@ -1,17 +1,92 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"net"
+	"net/http"
 	"os"
 	"os/signal"
+	"path"
 	"syscall"
-	"fmt"
 )
+
+type host struct {
+	IPAddress       string                 `json:"ip_address"`
+	LastCheckIn     string                 `json:"last_check_in"`
+	Port            int32                  `json:"port"`
+	Revision        string                 `json:"revision"`
+	Service         string                 `json:"service"`
+	ServiceRepoName string                 `json:"service_repo_name"`
+	Tags            map[string]interface{} `json:"tags"`
+}
+
+type registration struct {
+	Hosts   []host `json:"hosts"`
+	Env     string `json:"env"`
+	Service string `json:"service"`
+}
 
 func main() {
 	signalChannel := make(chan os.Signal, 1)
 	signal.Notify(signalChannel, syscall.SIGTERM, os.Interrupt)
 
 	fmt.Println("Server Started")
+
+	l, err := net.Listen("tcp", "127.0.0.1:8054")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, fmt.Sprintf("Address (%s) not available", "127.0.0.1:8054"))
+		os.Exit(1)
+	}
+
+	routes := map[string][]string{
+		"app-id.internal.local": {
+			"192.168.0.1",
+			"192.168.0.2",
+		},
+		"large-id.internal.local": {
+			"192.168.0.1",
+			"192.168.0.2",
+			"192.168.0.3",
+			"192.168.0.4",
+			"192.168.0.5",
+			"192.168.0.6",
+			"192.168.0.7",
+			"192.168.0.8",
+			"192.168.0.9",
+			"192.168.0.10",
+			"192.168.0.11",
+			"192.168.0.12",
+			"192.168.0.13",
+		},
+	}
+	go func() {
+		http.Serve(l, http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+			serviceKey := path.Base(req.URL.Path)
+
+			ips := routes[serviceKey]
+			hosts := []host{}
+			for _, ip := range ips {
+				hosts = append(hosts, host{
+					IPAddress: ip,
+					Tags:      make(map[string]interface{}),
+				})
+			}
+
+			var err error
+			json, err := json.Marshal(registration{Hosts: hosts})
+			if err != nil {
+				http.Error(resp, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			_, err = resp.Write(json)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Error writing to http response body") // not tested
+			}
+		}))
+	}()
+
 	select {
 	case <-signalChannel:
 		fmt.Println("Shutting service-discovery-controller down")
--- src/service-discovery-controller/main_test.go
+++ src/service-discovery-controller/main_test.go
@@ -1,12 +1,15 @@
 package main_test
 
 import (
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
-	"os"
 	"github.com/onsi/gomega/gexec"
-	"os/exec"
 )
 
 var _ = Describe("Main", func() {
@@ -34,4 +37,173 @@ var _ = Describe("Main", func() {
 		Eventually(session).Should(gbytes.Say("Shutting service-discovery-controller down"))
 	})
 
+	It("should return a http app json", func() {
+		Eventually(session).Should(gbytes.Say("Server Started"))
+
+		req, err := http.NewRequest("GET", "http://localhost:8054/v1/registration/app-id.internal.local", nil)
+		Expect(err).ToNot(HaveOccurred())
+		resp, err := http.DefaultClient.Do(req)
+		Expect(err).ToNot(HaveOccurred())
+		respBody, err := ioutil.ReadAll(resp.Body)
+		Expect(err).ToNot(HaveOccurred())
+
+		Expect(respBody).To(MatchJSON(`{
+			"env": "",
+			"hosts": [
+			{
+				"ip_address": "192.168.0.1",
+				"last_check_in": "",
+				"port": 0,
+				"revision": "",
+				"service": "",
+				"service_repo_name": "",
+				"tags": {}
+			},
+			{
+				"ip_address": "192.168.0.2",
+				"last_check_in": "",
+				"port": 0,
+				"revision": "",
+				"service": "",
+				"service_repo_name": "",
+				"tags": {}
+			}],
+			"service": ""
+		}`))
+	})
+
+	It("should return a http large json", func() {
+		Eventually(session).Should(gbytes.Say("Server Started"))
+
+		req, err := http.NewRequest("GET", "http://localhost:8054/v1/registration/large-id.internal.local", nil)
+		Expect(err).ToNot(HaveOccurred())
+		resp, err := http.DefaultClient.Do(req)
+		Expect(err).ToNot(HaveOccurred())
+		respBody, err := ioutil.ReadAll(resp.Body)
+		Expect(err).ToNot(HaveOccurred())
+
+		Expect(respBody).To(MatchJSON(`{
+			"env": "",
+			"hosts": [
+			{
+				"ip_address": "192.168.0.1",
+				"last_check_in": "",
+				"port": 0,
+				"revision": "",
+				"service": "",
+				"service_repo_name": "",
+				"tags": {}
+			},
+			{
+				"ip_address": "192.168.0.2",
+				"last_check_in": "",
+				"port": 0,
+				"revision": "",
+				"service": "",
+				"service_repo_name": "",
+				"tags": {}
+			},
+			{
+				"ip_address": "192.168.0.3",
+				"last_check_in": "",
+				"port": 0,
+				"revision": "",
+				"service": "",
+				"service_repo_name": "",
+				"tags": {}
+			},
+			{
+				"ip_address": "192.168.0.4",
+				"last_check_in": "",
+				"port": 0,
+				"revision": "",
+				"service": "",
+				"service_repo_name": "",
+				"tags": {}
+			},
+			{
+				"ip_address": "192.168.0.5",
+				"last_check_in": "",
+				"port": 0,
+				"revision": "",
+				"service": "",
+				"service_repo_name": "",
+				"tags": {}
+			},
+			{
+				"ip_address": "192.168.0.6",
+				"last_check_in": "",
+				"port": 0,
+				"revision": "",
+				"service": "",
+				"service_repo_name": "",
+				"tags": {}
+			},
+			{
+				"ip_address": "192.168.0.7",
+				"last_check_in": "",
+				"port": 0,
+				"revision": "",
+				"service": "",
+				"service_repo_name": "",
+				"tags": {}
+			},
+			{
+				"ip_address": "192.168.0.8",
+				"last_check_in": "",
+				"port": 0,
+				"revision": "",
+				"service": "",
+				"service_repo_name": "",
+				"tags": {}
+			},
+			{
+				"ip_address": "192.168.0.9",
+				"last_check_in": "",
+				"port": 0,
+				"revision": "",
+				"service": "",
+				"service_repo_name": "",
+				"tags": {}
+			},
+			{
+				"ip_address": "192.168.0.10",
+				"last_check_in": "",
+				"port": 0,
+				"revision": "",
+				"service": "",
+				"service_repo_name": "",
+				"tags": {}
+			},
+			{
+				"ip_address": "192.168.0.11",
+				"last_check_in": "",
+				"port": 0,
+				"revision": "",
+				"service": "",
+				"service_repo_name": "",
+				"tags": {}
+			},
+			{
+				"ip_address": "192.168.0.12",
+				"last_check_in": "",
+				"port": 0,
+				"revision": "",
+				"service": "",
+				"service_repo_name": "",
+				"tags": {}
+			},
+						{
+				"ip_address": "192.168.0.13",
+				"last_check_in": "",
+				"port": 0,
+				"revision": "",
+				"service": "",
+				"service_repo_name": "",
+				"tags": {}
+			}
+			],
+			"service": ""
+		}`))
+	})
 })
