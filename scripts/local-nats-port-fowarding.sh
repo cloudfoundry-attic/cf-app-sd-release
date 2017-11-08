2030857d02392e9b842ae8ec860e98613f12ec85
Joachim Valdez <valdezj@us.ibm.com>
2017-11-07 16:53:04 -0800

    Fix up script to setup ssh tunneling
    [#151407456]
    
    Signed-off-by: Dennis Leon <dleon@pivotal.io>

--- scripts/local-nats-port-fowarding.sh
+++ scripts/local-nats-port-fowarding.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
 
-ssh -L 8822:10.0.16.19:8222 -o "GatewayPorts yes" -l jumpbox  -i /tmp/toque-ssh-key 104.196.54.192
-ssh -L 8222:10.0.16.19:8222 -l bosh_09ee8a23605b4d5 -i /tmp/key 10.0.16.19
-
-
-ssh -L 5222:10.0.16.19:4222 -l jumpbox  -i /tmp/toque-ssh-key 104.196.54.192
-ssh -L 4222:10.0.16.19:4222 -l bosh_09ee8a23605b4d5 -i /tmp/key 10.0.16.19
-
-
-
-# localhost ports: 8822 && 5222
+ssh -L 8222:10.0.16.19:8222 -o "GatewayPorts yes" -l jumpbox  -i /tmp/toque-ssh-key 104.196.54.192
+ssh -L 4222:10.0.16.19:4222 -l jumpbox  -i /tmp/toque-ssh-key 104.196.54.192
